c3b6d1c9a60f98e796330b160f3200ead862824e
kazact <kazact95@gmail.com>
2026-09-20 09:29:18 -0500

    Update TODO

--- toolkit/graphics.py
+++ toolkit/graphics.py
@@ -6,12 +6,9 @@ My goal is to create a very rudimentary graphics library I can use for the rest 
 learn about packaging a library for distribution.
 
 TODO:
-    * Turn remaining TODOs into issues.
-    * Dynamic aspect to maintain square canvas(height/0.76 = width * .76)
-    * Consider efficiency gains like color runs, don't draw if 2 rows empty, draw full block if 2 rows same, ect.
     * Considering using new and old circle methods as a way to learn a how to benchmark and profile exactly where the
         gains come from.
-    * Start researching Mike Pitteway and drawing ellipses.
+
 """
 import sys
 from math import sqrt
